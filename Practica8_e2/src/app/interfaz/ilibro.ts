3cf0639099e352bb638184367193b03382dd1df1
BARBACID <39006181+ENTORNOSBARBACID@users.noreply.github.com>
2025-02-13 10:46:34 +0100

    Delete Practica8_e2/src directory

--- Practica8_e2/src/app/interfaz/ilibro.ts
+++
@@ -1,9 +0,0 @@
-export interface Ilibro {
-    titulo: string;
-    autor: string;
-    portada: string;
-    sinopsis:string;
-    genero:string;
-    editorial:string;
-    año_publicacion:number;
-}
