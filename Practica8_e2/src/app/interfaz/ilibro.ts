0e74d301cdade27f3ca96c89a3cadd4a56d0cf86
BARBACID <39006181+ENTORNOSBARBACID@users.noreply.github.com>
2025-02-13 10:47:04 +0100

    Add files via upload

---
+++ Practica8_e2/src/app/interfaz/ilibro.ts
@@ -0,0 +1,9 @@
+export interface Ilibro {
+    titulo: string;
+    autor: string;
+    portada: string;
+    sinopsis:string;
+    genero:string;
+    editorial:string;
+    año_publicacion:number;
+}
